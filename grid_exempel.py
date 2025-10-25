d5f3710d5653ecbd8144cdd32f8469c00b7d12be
Talha-Oo <off.bajwatalha@gmail.com>
2025-10-25 13:49:37 +0200

    COMPLETED WINS DIAOGNALLY - (RIGHT UP AND LEFT DOWN)

--- grid_exempel.py
+++ grid_exempel.py
@@ -91,12 +91,12 @@ def is_win(board, coordinates): #requires the grid/board and the current coordin
             match += 1 #add 1 to match
             current_col -= 1 #the column before that one
         
-    if(col+1 < 7):
+    if(col+1 < c):
         #if there is a column before the column user placed their token on
         
         current_col = col+1 #index of column after the on user placed their token on
         
-        while(current_col < 7 and token in board[row][current_col] and match != 4):
+        while(current_col < c and token in board[row][current_col] and match != 4):
             #while there still is a column after the one checked, check if it matches the players token
             #if yes;
                        
@@ -123,18 +123,56 @@ def is_win(board, coordinates): #requires the grid/board and the current coordin
             current_row -= 1 #the row before that one
         
     
-    if(row+1 <= 6):
+    if(row+1 <= r):
         #if there is a row before the row user placed their token on
         
         current_row = row+1 #index of row before the on user placed their token on
         
-        while(current_row <= 6 and token in board[current_row][col] and match != 4):
+        while(current_row <= r and token in board[current_row][col] and match != 4):
             #while there still is a row after the one checked, check if it matches the players token
             #if yes;
                        
             match += 1 #add 1 to match
             current_row += 1 #the ro w after that one            
+                        
+    if(match == 4):
+        
+        return True
+    
+    
+    match = 1 #rests the match variable
+    
+    #HANDLES WINS DIAOGNALLY - (RIGHT UP AND LEFT DOWN)
+    if(col-1 >= 0 and row+1 <= r):
+        #if there is a row under and a column behind the box the user placed their token in 
+        
+        current_col = col-1
+        current_row = row+1
+        
+        while((current_col >= 0 and current_row <= r) and token in board[current_row][current_col] and match != 4):
+            #while there is a row under and a column behind the box the user placed their tocken in 
+            #check if that has the token
             
+            match += 1 
+            
+            current_col -= 1
+            current_row += 1
+            #go to the next coordinate, the row under and the column before
+    
+    if(col+1 < c and row-1 >= 1):
+        #if there is a row above and a column after the box the user placed their token in 
+        
+        current_col = col+1
+        current_row = row-1
+        
+        while((current_col < c and current_row >= 1) and token in board[current_row][current_col] and match != 4):
+            #while there is a row above and a column after the box the user placed their tocken in 
+            #check if that has the token        
+            match += 1 
+            
+            current_col += 1
+            current_row -= 1          
+            #go to the next coordinate, the row above and the column after
                 
     if(match == 4):
         
